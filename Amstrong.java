b111899b57eea4c3677642f7c6ddcaa52af290c7
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-06-23 09:57:04 +0530

    Create Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Amstrong 
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int c=a;
+        int sum=0;
+        while(a>0)
+        {
+           int b=a%10;
+           sum=sum+(b*b*b);
+           a=a/10;
+        }
+        if(c==sum)
+        System.out.print("yes");
+        else
+        System.out.print("no");
+    }
+}
